4c363bea534510432f77bea1f2fb550f3be581a7
benhohner <benhohner@gmail.com>
2024-12-17 22:39:55 -0500

    change key exchange types

--- src/2-process/1-utility/1-universal/SecureEvents.ts
+++ src/2-process/1-utility/1-universal/SecureEvents.ts
@@ -5,8 +5,8 @@ import type { AppEvent } from './BaseEvents';
 export interface SecureEventMap {
   SECURE_STORE_ACTION: SignedMessage<StoreAction>;
   SECURE_STORE_UPDATE: SignedMessage<StoreUpdate<any>>;
-  SERVER_KEY_EXCHANGE: { publicKey: CryptoKey };
-  CLIENT_KEY_EXCHANGE: SignedMessage<{ publicKey: CryptoKey }>;
+  SERVER_KEY_EXCHANGE: { publicKey: JsonWebKey };
+  CLIENT_KEY_EXCHANGE: SignedMessage<{ publicKey: JsonWebKey }>;
   REQUEST_INITIAL_STATE: SignedMessage<{ clientId: string }>;
   INITIAL_STATE: SignedMessage<StoreUpdate<any>>;
 }
